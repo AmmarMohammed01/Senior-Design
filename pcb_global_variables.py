529335d0a61fb1a0b4e4399fdabce98f436cf733
Ammar Mohammed <118853010+AmmarMohammed01@users.noreply.github.com>
2026-04-28 23:21:37 -0500

    Committing stash changes of ringlight code.

--- pcb_global_variables.py
+++ pcb_global_variables.py
@@ -4,3 +4,5 @@ SCRIPT_DIR = Path(__file__).parent.resolve()
 BOARDS_DIR = SCRIPT_DIR / "boards"
 board_type = ""
 selected_board_dir = BOARDS_DIR / board_type # not sure if this get recalculated after each time board_type is changed
+
+LIGHTS_AVAILABLE = False
--- ringlight_code.py
+++ ringlight_code.py
@@ -1,46 +1,54 @@
-import board
-import neopixel
+import pcb_global_variables as gv
 
-# Configuration
-PIXEL_PIN_TOP = board.D18   # GPIO18 - Top Ringlight
-PIXEL_PIN_BOTTOM = board.D12 # GPIO12 - Bottom Ringlight
-NUM_PIXELS = 16
-BRIGHTNESS = 0.1        # 0.0 to 1.0
+try:
+    import board
+    import neopixel
+    gv.LIGHTS_AVAILABLE = True
 
-# Color constants
-WHITE_COLOR = (255,255,255)
-NO_COLOR = (0,0,0)
+    # Configuration
+    PIXEL_PIN_TOP = board.D18   # GPIO18 - Top Ringlight
+    PIXEL_PIN_BOTTOM = board.D12 # GPIO12 - Bottom Ringlight
+    NUM_PIXELS = 16
+    BRIGHTNESS = 0.3        # 0.0 to 1.0
 
-# Initialize NeoPixel
-top_pixels = neopixel.NeoPixel(
-    PIXEL_PIN_TOP,
-    NUM_PIXELS,
-    brightness=BRIGHTNESS,
-    auto_write=False
-)
+    # Color constants
+    WHITE_COLOR = (255,255,255)
+    NO_COLOR = (0,0,0)
 
-bottom_pixels = neopixel.NeoPixel(
-    PIXEL_PIN_BOTTOM,
-    NUM_PIXELS,
-    brightness=BRIGHTNESS,
-    auto_write=False
-)
+    # Initialize NeoPixel
+    top_pixels = neopixel.NeoPixel(
+        PIXEL_PIN_TOP,
+        NUM_PIXELS,
+        brightness=BRIGHTNESS,
+        auto_write=False
+    )
 
-def turn_on(light_name: str):
-    '''Light name = "top" or "bottom"'''
-    if light_name == "top":
-        top_pixels.fill(WHITE_COLOR)
-        top_pixels.show()
-    elif light_name == "bottom":
-        bottom_pixels.fill(WHITE_COLOR)
-        bottom_pixels.show()
+    bottom_pixels = neopixel.NeoPixel(
+        PIXEL_PIN_BOTTOM,
+        NUM_PIXELS,
+        brightness=BRIGHTNESS,
+        auto_write=False
+    )
 
-def turn_off(light_name: str):
-    '''Light name = "top" or "bottom"'''
-    if light_name == "top":
-        top_pixels.fill(NO_COLOR)
-        top_pixels.show()
-    elif light_name == "bottom":
-        bottom_pixels.fill(NO_COLOR)
-        bottom_pixels.show()
+    def turn_on(light_name: str):
+        '''Light name = "top" or "bottom"'''
+        if light_name == "top":
+            top_pixels.fill(WHITE_COLOR)
+            top_pixels.show()
+        elif light_name == "bottom":
+            bottom_pixels.fill(WHITE_COLOR)
+            bottom_pixels.show()
+
+    def turn_off(light_name: str):
+        '''Light name = "top" or "bottom"'''
+        if light_name == "top":
+            top_pixels.fill(NO_COLOR)
+            top_pixels.show()
+        elif light_name == "bottom":
+            bottom_pixels.fill(NO_COLOR)
+            bottom_pixels.show()
+
+except (ImportError, ModuleNotFoundError):
+    gv.LIGHTS_AVAILABLE = False
+    print("Could not find 'board' and 'neopixel' Python libraries to setup lighting")
 
