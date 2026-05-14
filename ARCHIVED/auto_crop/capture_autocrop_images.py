c85284f5e149b726e79eef536bc6c97912f3b223
Ammar Mohammed <example>
2026-05-13 22:37:33 -0500

    Code to help obtain autocrop images

--- ARCHIVED/auto_crop/capture_autocrop_images.py
+++ ARCHIVED/auto_crop/capture_autocrop_images.py
@@ -1,5 +1,29 @@
 import cv2 as cv
 
+import board
+import neopixel
+import json
+
+PIXEL_PIN_TOP = board.D12
+NUM_PIXELS = 16
+BRIGHTNESS = 0.3
+WHITE_COLOR = (255,255,255)
+NO_COLOR = (0,0,0)
+
+top_pixels = neopixel.NeoPixel(
+    PIXEL_PIN_TOP,
+    NUM_PIXELS,
+    brightness=BRIGHTNESS,
+    auto_write=False
+)
+def turn_on():
+    top_pixels.fill(WHITE_COLOR)
+    top_pixels.show()
+
+def turn_off():
+    top_pixels.fill(NO_COLOR)
+    top_pixels.show()
+
 camera_index = 0
 
 cap = cv.VideoCapture(camera_index)
@@ -9,12 +33,14 @@ cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
 if not cap.isOpened():
     print("Cannot open camera")
-    return
+    exit()
 
 
 current_w = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 current_h = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
 print(f"Current Resolution: {int(current_w)}x{int(current_h)}")
+
+LIGHT_ON = False
 
 image_num = 1
 while True:
@@ -32,6 +58,14 @@ while True:
     if key == ord('c'):
         print(f'Image Captured {filename}')
         cv.imwrite(filename, frame)
+        image_num += 1
+
+    if key == ord('l'):
+        LIGHT_ON = not LIGHT_ON
+        if LIGHT_ON:
+            turn_on()
+        else:
+            turn_off()
 
     if key == ord('q'):
         break
